5efb4b6232b9f66f123686203c74644d800256c7
Manpreet <manpreet_kaur.singh@etu.sorbonne-universite.com>
2024-04-29 09:47:19 +0200

    code pour troupe

--- troupe.cpp
+++ troupe.cpp
@@ -12,21 +12,37 @@ Troupe::Troupe(int niv){
 // TRAVAILLEUR
 
 int Travailleur::chercher_ressources(){ // Renvoie le nombre de ressources collectées (en fonction de son niveau)
-
+    int nb_ressources = 0;
+    nb_ressources = _niveau * 5 ;
+    std::cout << "Le nombre de ressources collectees est de " << nb_ressources << std::endl ;
+    return nb_ressources;
 }
 
-void Travailleur::reparer_batiment(int ressources){ // Répare un batiment en fonction des ressources du joueur
-
+void Travailleur::reparer_batiment(int ressources, Batiment& batiment){ // Répare un batiment en fonction des ressources du joueur
+    if (ressources >= _niveau * 5 && batiment.getvie() < VIE_MAX_BAT) {
+        batiment.getvie() += _niveau * 2 ;
+        ressources += _niveau * 5 ;
+    }
+    else {
+        std::cout << "Le batiment a atteint son niveau max ou le nombre de ressources n'est pas suffisant" << std::endl ;
+    }
 }
 
 // ----------------------------------------------------------------
 // TROUPE DE GUERRE
 
-void TroupeDeGuerre::attaquer_troupe(Troupe &troupe){ // On attaque une troupe (dépend du niveau de la troupe)
-
-}
-
 void TroupeDeGuerre::attaquer_batiment(Batiment &batiment){ // On attaque un batiment (dépend du niveau de la troupe)
+    std::cout << "Le batiment avait une vie de " << batiment.getvie() << std::endl;
+    batiment.getvie() += -(_niveau / 2 * 20);
+    
+    if (batiment.getvie() <= 0 ){
+        batiment.getvie() = 0 ;
+        std::cout << "Le batiment a été detruit." << std::endl;
+    }
+
+    else {
+        std::cout << "Le batiment a maintenant une vie de " << batiment.getvie() << std::endl;
+    }
 
 }
 
@@ -35,6 +51,22 @@ void TroupeDeGuerre::attaquer_batiment(Batiment &batiment){ // On attaque un bat
 
 Soldat::Soldat(int niveau) : Troupe(niveau){
     _vie = _niveau/2*60;
+    std::cout << "Les soldats ont une vie de " << _vie << std::endl ;
+}
+
+void Soldat::attaquer_troupe(Troupe &troupe){ // On attaque une troupe (dépend du niveau de la troupe)
+    std::cout << "La troupe adverse avait une vie de " << troupe.getvie() << std::endl;
+    troupe.getvie() += -(_niveau / 2 * 20);
+    
+    if (troupe.getvie() <= 0 ){
+        troupe.getvie() = 0 ;
+        std::cout << "La troupe adverse est morte." << std::endl;
+    }
+
+    else {
+        std::cout << "La troupe adverse a maintenant une vie de " << troupe.getvie() << std::endl;
+    }
+
 }
 
 void Soldat::defendre_batiment(Batiment batiment){ // La troupe défend un batiment
@@ -46,8 +78,32 @@ void Soldat::defendre_batiment(Batiment batiment){ // La troupe défend un batim
 
 Magicien::Magicien(int niveau) : Troupe(niveau){
     _vie = _niveau/2*30;
+    std::cout << "Les magiciens ont une vie de " << _vie << std::endl;
+}
+soins = niveau / 2 * 10
+void Magicien::attaquer_troupe(Troupe &troupe){ // On attaque une troupe (dépend du niveau de la troupe)
+    std::cout << "La troupe adverse avait une vie de " << troupe.getvie() << std::endl;
+    troupe.getvie() += -(_niveau / 2 * 25);
+    
+    if (troupe.getvie() <= 0 ){
+        troupe.getvie() = 0 ;
+        std::cout << "La troupe adverse est morte." << std::endl;
+    }
+
+    else {
+        std::cout << "La troupe adverse a maintenant une vie de " << troupe.getvie() << std::endl;
+    }
+
 }
 
 void Magicien::soigner(Troupe &troupe){ // On soigne la troupe en parametre (dépend du niveau du magicien)
+    if (troupe.getvie() <= VIE_MAX_TROUPE - _niveau / 2 * 10) {
+        troupe.getvie() += _niveau / 2 * 10 ;
+    }
 
+    else {
+        troupe.getvie() = VIE_MAX_TROUPE ;
+    }
+
+    std::cout << "La troupe soignée a maintenant une vie de " << troupe.getvie() << std::endl;
 }
--- troupe.hpp
+++ troupe.hpp
@@ -10,20 +10,20 @@ class Troupe{
         std::string _type_troupe;
     public : 
         Troupe(int niv);
+        int getniveau(){return _niveau;}
 };
 
 class Travailleur : public Troupe{
     public:
         Travailleur(int niveau) : Troupe(niveau){} // Constructeur de travailleur (on affecte le niveau)
         int chercher_ressources(); // Renvoie le nombre de ressources collectées (en fonction de son niveau)
-        void reparer_batiment(int ressources); // Répare un batiment en fonction des ressources du joueur
+        void reparer_batiment(int ressources, Batiment& batiment); // Répare un batiment en fonction des ressources du joueur
 };
 
 class TroupeDeGuerre : public Troupe{
     protected:
         int _vie;
     public:
-        void attaquer_troupe(Troupe &troupe); // On attaque une troupe (dépend du niveau de la troupe)
         void attaquer_batiment(Batiment &batiment); // On attaque un batiment (dépend du niveau de la troupe)
         void se_fait_soigner(int qtt_soin){ _vie += qtt_soin;} // La troupe se fait soigner d'une quantité définie
 };
@@ -31,11 +31,13 @@ class TroupeDeGuerre : public Troupe{
 class Soldat : public TroupeDeGuerre{
     public:
         Soldat(int niveau);
+        void attaquer_troupe(Troupe &troupe); // On attaque une troupe (dépend du niveau de la troupe)
         void defendre_batiment(Batiment batiment); // La troupe défend un batiment
 };
 
 class Magicien : public TroupeDeGuerre{
     public:
+        void attaquer_troupe(Troupe &troupe); // On attaque une troupe (dépend du niveau de la troupe)
         void soigner(Troupe &troupe); // On soigne la troupe en parametre (dépend du niveau du magicien)
 };
 
